fdcaf4be4e0f09b5e8cefb90bc95f8be56fbcb74
zhugaowei <zhugaowei@foxmial.com>
2018-07-15 17:24:11 +0800

    my Django app, first commit

---
+++ df_user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=40)
+    uemail = models.CharField(max_length=30)
+    ushou = models.CharField(max_length=20)
+    uaddress = models.CharField(max_length=100)
+    uyoubian = models.CharField(max_length=6)
+    uphone = models.CharField(max_length=11)
